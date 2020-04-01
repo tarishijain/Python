c7b2dbceebe5b435774ff25451dda3937ddf3106
Tarishi Jain <62653356+tarishijain@users.noreply.github.com>
2020-04-02 04:45:51 +0530

    Update AlphaSort.py

--- AlphaSort.py
+++ AlphaSort.py
@@ -1,17 +1,8 @@
-dict={"student1":"Aman","student2":"Dheeraj","student3":"Mohit","student4":"Kanika","student5":"Amrita"}
+dict1={"student1":"Aman","student2":"Dheeraj","student3":"Mohit","student4":"Kanika","student5":"Amrita"}
+print(dict1)
 list1=[]
-list2=[]
-for key,value in dict.items():
-    list1.append(key)
-    list2.append(value)
-list2.sort()
-dict1={}
-for x in list1:
-    for y in list2:
-        dict1[x]=y
-        list2.remove(y)
-        break
-
-
-print(dict1)    
-
+for i in dict1:
+    list1.append(dict1[i])
+list1.sort()
+c = dict(zip(dict1.keys(),list1)) #to convert lists to dict. 
+print(c) 
